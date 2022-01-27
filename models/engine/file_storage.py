ccf77f894a5ec39e51a43778f7092950a4ee1f63
sequainoo <wazatoyung@gmail.com>
2022-01-27 11:39:37 +0000

    Add FileStorage class definitions

---
+++ models/engine/file_storage.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+'''
+File storage.
+Provides FileStorage class to store and retrive instances/objects
+'''
+
+
+class FileStorage():
+    '''File storage class.
+
+    Serializes instances to JSON file.
+    Deserializes JSON file to instances.
+
+    '''
+    #: path to the JSON file ex: file.json
+    __file_path = ''
+
+    #: Stores all objects by <class name>.id ex: BaseModel.12121212
+    __objects = {}
+
+    def all(self):
+        '''Returns the dictionary __objects.'''
+        pass
+    
+    def new(self, obj):
+        '''Sets in __objects the obj with key <obj class name>.id.'''
+        pass
+    
+    def save(self):
+        '''Serializes __objects to the JSON file (path: __file_path)'''
+        pass
+    
+    def reload(self):
+        '''
+        Deserializes the JSON file to __objects only if the file exists.
+        Otherwise, do nothing.
+        '''
+        pass
