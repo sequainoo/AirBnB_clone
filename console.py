e1eb2a327a1e67e10b07c35e5941df39f545b0f5
sequainoo <wazatoyung@gmail.com>
2022-01-28 03:04:11 +0000

    Add create command to console

--- console.py
+++ console.py
@@ -3,6 +3,12 @@
 
 
 import cmd
+import models
+
+
+CLASSNAMES = {
+    'BaseModel': models.base_model.BaseModel
+}
 
 
 class HBNBCommand(cmd.Cmd):
@@ -10,19 +16,40 @@ class HBNBCommand(cmd.Cmd):
 
     prompt = '(hbnb) '
 
-    def do_quit(self, inp):
-        '''
-        Quits the console.
-        Usage: quit
+    def do_quit(self, arg):
+        '''Quit command to exit the console.
         '''
         return True
 
-    def do_EOF(self, inp):
-        '''
-        Quits the console.
-        Usage: ctrl + d.
+    def do_EOF(self, arg):
+        '''Quit command to exit the console.
         '''
         return True
+
+    def emptyline(line):
+        '''Empty lines executes nothing.'''
+        pass
+
+    def do_create(self, class_name):
+        '''Creates a new instance of BaseModel, saves it and prints the id.'''
+        if not class_name:
+            print('** class name missing **')
+        elif class_name not in CLASSNAMES:
+            print('** class doesn\'t exist ** ')
+        else:
+            instance = CLASSNAMES[class_name]()
+            instance.save()
+            print(instance.id)
+
+
+    # @staticmethod
+    # def alert_invalid_input(class_name):
+    #     '''Prints error message on invalid arguments.'''
+
+    # @staticmethod
+    # def exists(class_name, _id):
+    #     '''Checks for existence of instance with id and class name.'''
+    #     pass
 
 
 def main():
