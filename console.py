778c32955801a6761386a1bf936a16ca1443ec6e
sequainoo <wazatoyung@gmail.com>
2022-01-30 23:13:03 +0000

    ClassName.update(id, attr, val) implemented

--- console.py
+++ console.py
@@ -50,9 +50,8 @@ class HBNBCommand(cmd.Cmd):
             print(instance.id)
 
     def do_show(self, args):
-        '''Prints the str representation of an instance.
-
-        Based on class name and id.
+        '''Prints an instance based on class name and id.
+        show <class name> <id>
         '''
         args = self.parse_args_2(args)
         if args is None:
@@ -76,9 +75,8 @@ class HBNBCommand(cmd.Cmd):
         storage.save()
 
     def do_all(self, class_name):
-        '''Prints str representation of all instances.
-
-        Based on or not on class name.
+        '''Prints all instances based on or not on class name.
+        all [<class name>]
         '''
         _list = []
         if not class_name:
@@ -95,7 +93,9 @@ class HBNBCommand(cmd.Cmd):
             print('** class doesn\'t exist **')
 
     def do_update(self, args):
-        '''Updates an instance based on class name and id.'''
+        '''Updates an instance based on class name and id.
+        update <class name> <id> <attr name> "<attr value>"
+        '''
         args = self.parse_args_for_update(args)
         if args is None:
             return
@@ -103,13 +103,21 @@ class HBNBCommand(cmd.Cmd):
         search_key = self.key_from_storage(class_name, _id)
         obj = self.find_obj(search_key)
         if obj is None:
+            print('** no instance found **')
             return
         existing_attr = getattr(obj, attr_name, None)
         if existing_attr is None:
             setattr(obj, attr_name, attr_value)
         else:
-            attr_value = type(existing_attr)(attr_value)
-            setattr(obj, attr_name, attr_value)
+            _class = type(existing_attr)
+            if _class is list:
+                if attr_value not in existing_attr:
+                    tmp_list = list(existing_attr)
+                    tmp_list.append(attr_value)
+                    setattr(obj, attr_name, tmp_list)
+            else:
+                attr_value = _class(attr_value)
+                setattr(obj, attr_name, attr_value)
         storage.save()
 
     def default(self, line):
@@ -118,17 +126,19 @@ class HBNBCommand(cmd.Cmd):
 
         # if cmd not like <cls name>.all() delegate to super().default()
         if cmd.endswith('.all()'):
-            self.class_all(cmd.split('.')[0]) 
+            self.class_dot_all(cmd.split('.')[0]) 
         elif cmd.endswith('.count()'):
-            self.class_count(cmd.split('.')[0])
+            self.class_dot_count(cmd.split('.')[0])
         elif self.is_class_dot_show(cmd):  # if it is classname.show() command
-            self.class_dot_show(*self.parse_class_dot_show_args(cmd))
+            self.class_dot_show()
         elif self.is_class_dot_destroy(cmd):  # if is ClassName.destroy()
-            self.class_dot_destroy(*self.parse_class_dot_destroy_args(cmd))
+            self.class_dot_destroy(cmd)
+        elif self.is_class_dot_update(cmd):
+            self.class_dot_update(cmd)
         else:
             super().default(line)
 
-    def class_all(self, class_name):
+    def class_dot_all(self, class_name):
         '''Gets all instances of class.'''
         if class_name in CLASSNAMES:
             _list = [
@@ -139,7 +149,7 @@ class HBNBCommand(cmd.Cmd):
         else:
             super().default(class_name)
     
-    def class_count(self, class_name):
+    def class_dot_count(self, class_name):
         '''Count number of instances of a class.'''
         if class_name in CLASSNAMES:
             count = 0
@@ -150,8 +160,9 @@ class HBNBCommand(cmd.Cmd):
         else:
             super().default(class_name)
 
-    def class_dot_show(self, class_name, _id):
+    def class_dot_show(self, cmd):
         '''Retrieve an instance based on its id.'''
+        class_name, _id = self.parse_class_dot_show_args(cmd)
         if class_name == '':
             print('** class name missing **')
         elif _id == '':
@@ -184,7 +195,8 @@ class HBNBCommand(cmd.Cmd):
         return class_name, _id
 
     def class_dot_destroy(self, class_name, _id):
-        '''Retrieve an instance based on its id.'''
+        '''Destroys an instance based on its id.'''
+        class_name, _id = self.parse_class_dot_destroy_args(cmd)
         if class_name == '':
             print('** class name missing **')
         elif _id == '':
@@ -196,6 +208,37 @@ class HBNBCommand(cmd.Cmd):
                 storage.save()
             else:
                 print('** no instance found **')
+
+    def class_dot_update(self, cmd):
+        '''Handles ClassName.update() commands
+        <class name>.update(<id>, <attribute name>, <attribute value>).
+        '''
+        cls_name, _id, attr_name, attr_value = self.parse_class_dot_update(cmd)
+        search_key = self.key_from_storage(cls_name, _id)
+        args = '{} {} {} "{}"'.format(cls_name, _id, attr_name, attr_value)
+        self.do_update(args)
+
+    def parse_class_dot_update(self, cmd):
+        '''Parses
+        ClassName.update(id, attribute_name, "attribute_value").
+        '''
+        args = cmd.split('.')
+        cls_name = args[0]
+        part_2 = args[1].replace('update(', '').replace(')', '')
+        num_commas = part_2.count(',')
+        if num_commas < 2:
+            return cls_name, None, None, None
+        elif num_commas == 2:
+            _id, attr_name, attr_value = part_2.split(',')[0:]
+            _id = _id.strip()
+            attr_name = attr_name.strip()
+            attr_value = attr_value.strip().strip('"')
+            return cls_name, _id, attr_name, attr_value
+        else:
+            pass
+        # tmp_l = part_2.split(',')
+        # if len()
+        return cls_name, None, None, None
 
     @staticmethod
     def is_class_dot_show(cmd):
@@ -228,11 +271,25 @@ class HBNBCommand(cmd.Cmd):
         return False
 
     @staticmethod
+    def is_class_dot_update(cmd):
+        '''Checks if cmd is like any of the forms below.
+
+        <class name>.update(<id>, <attr name>, <attr value>).
+        <class name>.update(<id>, <dictionary representation>).
+        '''
+
+        if '.update(' in cmd and cmd[-1] == ')':
+            if cmd.count('(') == 1 and cmd.count(')') == 1:
+                if cmd.index('(') < cmd.index(')'):
+                    if not cmd.startswith('.'):
+                        return True
+        return False
+
+    @staticmethod
     def find_obj(key):
         return storage.all().get(key, None)
 
-    @staticmethod
-    def parse_args_for_update(args):
+    def parse_args_for_update(self, args):
         '''Parse args line <class name> <id> <attr name> "<attr value>".'''
         line = args
         if not args:
@@ -249,9 +306,13 @@ class HBNBCommand(cmd.Cmd):
             print('** value missing **')
             return
         class_name, _id, attr_name = args[:3]
-        start = line.index('"') + 1
-        end = line.index('"', start)
-        attr_value = line[start:end]
+        start = line.find('"') + 1
+        end = line.find('"', start)
+        if start == 0 or end == -1:
+            super().default(line)
+            return
+        else:
+            attr_value = line[start:end]
 
         if class_name not in CLASSNAMES:
             print('** class doesn\'t exist **')
