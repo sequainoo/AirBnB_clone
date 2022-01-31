57fe6c4ee796feb8e222e7600cbbe4b4c06342fe
sequainoo <wazatoyung@gmail.com>
2022-01-31 03:51:22 +0000

    Fix model imports

--- models/__init__.py
+++ models/__init__.py
@@ -1,5 +1,14 @@
 from .engine import file_storage
 
 
+__all__ = [
+    'amenity',
+    'base_model',
+    'city',
+    'place',
+    'review',
+    'state',
+    'user',
+    ]
 storage = file_storage.FileStorage()
 storage.reload()
