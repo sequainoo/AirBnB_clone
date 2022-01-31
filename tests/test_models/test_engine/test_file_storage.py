f14040c3e284800f783895a70bc37d441b48d7e0
sequainoo <wazatoyung@gmail.com>
2022-01-31 04:23:47 +0000

    Add tests for file_storage.py

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+'''Tests for file_storage module.'''
+
+
+import unittest
+
+
+class TestFileStorage(unittest.TestCase):
+    '''Tests for the units of FileStorage.'''
+
+    def test_all(self):
+        '''Tests all method.
+
+        The all method returns a list of all objects.
+        '''
+        pass
